4b99c1bf934b264d00247205bd2b51365eaaa9fa
Adnene KHALFA <adnene.khalfa@wistiki.com>
2015-12-15 01:03:48 +0100

    added Resource class to lib index

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@
 require('source-map-support').install();
 import acl from './lib/acl';
 import role from './lib/role';
+import resource from './lib/resource';
 
 
 export {acl as Acl};
-export {role as Role}
+export {role as Role};
+export {resource as Resource};
